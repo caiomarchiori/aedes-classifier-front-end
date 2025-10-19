e90801aa680b0541bb3e8fe309c6722fa7f39102
Caio Marchiori <caiorabottinimarchiori@gmail.com>
2025-10-19 15:40:07 -0300

    first commit

---
+++ Desktop/aedes-classificator-front-end/aedes-classifier-front-end/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-900">
+        Tailwind funcionando!
+      </h1>
+    </div>
+  );
+}
