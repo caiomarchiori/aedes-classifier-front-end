224d2eed804acf85f5420858ab2685f894ea0a9e
Caio Marchiori <caiorabottinimarchiori@gmail.com>
2026-03-07 15:29:39 -0300

    footer

--- Desktop/aedes-classificator-front-end/aedes-classifier-front-end/src/components/footer.tsx
+++ Desktop/aedes-classificator-front-end/aedes-classifier-front-end/src/components/footer.tsx
@@ -1,36 +1,43 @@
 "use client";
+import { FaLinkedin, FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 
 export function Footer() {
 
     const navLinks = [
-        { label: "LinkedIn", href: "https://www.linkedin.com" },
-        { label: "Facebook", href: "https://www.facebook.com" },
-        { label: "Instagram", href: "https://www.instagram.com" },
-        { label: "Twitter", href: "https://www.twitter.com" },
-        { label: "YouTube", href: "https://www.youtube.com" }
+        { label: "LinkedIn", href: "https://www.linkedin.com/in/caio-marchiori-808994263", icon: FaLinkedin },
+        { label: "Facebook", href: "https://web.facebook.com/minsaude?_rdc=1&_rdr#", icon: FaFacebook },
+        { label: "Instagram", href: "https://www.instagram.com/minsaude", icon: FaInstagram },
+        { label: "YouTube", href: "https://www.youtube.com/minsaudebr", icon: FaYoutube }
     ];
 
 
     return (
-        <div className="w-full bg-[#2F7AF0] rounded-sm px-12 py-4 flex items-center justify-between font-semibold">
+        <div className="w-full bg-[#2F7AF0] rounded-sm pl-12 py-4 flex items-center space-x-10 font-semibold">
 
             <p className="text-white">Redes Sociais</p>
 
-            <div className="w-0.5 h-12 bg-[#E2E2E2]" />
+            <div className="w-0.5 h-10 bg-[#E2E2E2]" />
 
             <div className="flex items-center space-x-12 text-white font-semibold">
 
-                {navLinks.map((link) => (
-                    <a
-                        key={link.href}
-                        href={link.href}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="transition-colors hover:text-blue-200"
-                    >
-                        {link.label}
-                    </a>
-                ))}
+                {navLinks.map((link) => {
+                    const Icon = link.icon;
+                    return (
+                        <div className="flex items-center space-x-2 transition-colors hover:text-blue-200">
+                            <div className="rounded-full p-1 bg-blue-600">
+                                <Icon size={20} />
+                            </div>
+                            <a
+                                key={link.href}
+                                href={link.href}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                {link.label}
+                            </a>
+                        </div>
+                    )
+                })}
 
             </div>
             <div className="w-0.5 h-12 bg-[#E2E2E2]" />
